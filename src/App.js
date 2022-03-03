ec8c48064eaa38b28e466bed82ab17272ec6b595
William Rainey <williamhrainey@gmail.com>
2022-03-03 11:56:48 -0800

    added some css to style the stuff

--- src/App.js
+++ src/App.js
@@ -8,21 +8,9 @@ function App() {
       <header className="App-header">
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarBrand id="brand" href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
