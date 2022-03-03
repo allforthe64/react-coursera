7298c216bdd8b6e3555ccb460926880517cfb75e
William Rainey <williamhrainey@gmail.com>
2022-03-03 11:35:12 -0800

    added reactstrap import and modded app.JS to contain navbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'boostrap/dist/css/boostrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
